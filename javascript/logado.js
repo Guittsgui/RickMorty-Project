7f4098225cc90b315800286d6656ad1106497307
Guilherme Tavares <guilhermet.silva@viannasempre.com.br>
2023-02-09 23:04:19 -0300

    pequenas mudancas

---
+++ javascript/logado.js
@@ -0,0 +1,6 @@
+const btQuizz = document.querySelector('.btQuizz').addEventListener('click',quizzActive)
+const homePage = document.querySelector('.homepage')
+
+function quizzActive(){
+    homePage.style.display = 'none'
+}
