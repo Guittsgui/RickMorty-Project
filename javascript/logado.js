3be2fcd42a7cba9b456ad4df1e22f900787d4a29
Guilherme Tavares <guilhermet.silva@viannasempre.com.br>
2023-02-15 10:14:53 -0300

    colocando todos os eps

--- javascript/logado.js
+++ javascript/logado.js
@@ -33,8 +33,12 @@ function homeAction(){
 async function getEpisodes(){
     let req = await fetch('https://rickandmortyapi.com/api/episode')
     let json = await req.json()
-    let listaEp = json.results
-    for ( ep of listaEp){
+    let reqp2 = await fetch('https://rickandmortyapi.com/api/episode?page=2')
+    let json2 = await reqp2.json()
+    let reqp3 = await fetch('https://rickandmortyapi.com/api/episode?page=3')
+    let json3 = await reqp3.json()
+    let listaGeral = json.results.concat(json2.results, json3.results)
+    for ( ep of listaGeral){
         const divMaior = document.createElement('div')
         divMaior.classList.add('epBox')
         divMaior.innerHTML = `<h2>${ep.episode} </h2>  <h3> Nome:</h3> <p>${ep.name}</p> <h3>Lançamento: </h3> <p>${ep.air_date} </p>`
